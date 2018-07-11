0ac7fc7f47d27da2db3e88294f6b03b372eda18a
HUOBIINC\liming0114 <liming@huobi.com>
2018-07-11 20:55:58 +0800

    调整SDK

--- src/main/java/com/sdk/OpenSDk.java
+++ src/main/java/com/sdk/OpenSDk.java
@@ -7,6 +7,7 @@ import com.sdk.utils.HttpClient;
 import com.sdk.utils.LoggerUtil;
 
 import org.apache.commons.lang.RandomStringUtils;
+import org.apache.commons.lang.StringUtils;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.methods.HttpRequestBase;
@@ -26,74 +27,46 @@ public class OpenSDk {
     private static final int Request_Post = 1;
     private static  String SERVER_ADDRESS;
     private static final String PayPrepay = "/payment/prepay";
-    private static final String QueryCode = "/appUsers/getLoginCode";
     private static final String QueryOpenid = "/appUsers/getOpenid";
+    private static String APPUID;
+    private static String APPSECRET;
+    private static String ACCOUNTTTOKEN;
 
     /**
      * 初始化服务地址和端口
-     *
-     * @param serveraddress
-     *            必选参数 服务器地址
+     * @param serveraddress  服务器地址
+     * @param appUid         APPID
+     * @param accountToken   TOKEN
+     * @param appSecret      SECRET
      */
-    public void init(String serveraddress) {
-        if (isEmpty(serveraddress)) {
+    public void init(String serveraddress,String appUid,String accountToken,String appSecret) {
+        if (StringUtils.isEmpty(serveraddress)) {
             LoggerUtil.error("初始化异常:serveraddress");
-            throw new IllegalArgumentException("必选参数:" + serveraddress +") "+ "为空");
+            throw new IllegalArgumentException("必选参数:serveraddress为空");
+        }
+        if (StringUtils.isEmpty(accountToken)) {
+            LoggerUtil.error("初始化异常:accountToken");
+            throw new IllegalArgumentException("必选参数:accountToken 为空");
+        }
+        if (StringUtils.isEmpty(accountToken)) {
+            LoggerUtil.error("初始化异常:appSecret");
+            throw new IllegalArgumentException("必选参数:appSecret 为空");
         }
         SERVER_ADDRESS = serveraddress;
-    }
-
-
-    private boolean isEmpty(String str) {
-        return (("".equals(str)) || (str == null));
-    }
-
-
-    /**
-     * 获取用户登陆凭证
-     * @param appUid       第三方应用APP_ID
-     * @param accountUid   App账号ID
-     * @param accountToken App账号当前Token
-     * @return
-     */
-    public  String  queryCode(String appUid, String accountUid, String accountToken){
-
-        Map<String, Object> params = new HashMap<String, Object>();
-        params.put("appUid", appUid);
-        params.put("accountUid", accountUid);
-        params.put("accountToken", accountToken);
-        String ret = "";
-        String jsonParam= JSONObject.toJSONString(params);
-        try{
-            ret = HttpClient.postDataJson(SERVER_ADDRESS+QueryCode,jsonParam);
-        }catch (Exception e) {
-            e.printStackTrace();
-            LoggerUtil.error(e.getMessage());
-            return getMyError("101111", "请求异常");
-        }
-        return ret;
+        APPUID = appUid;
+        APPSECRET = appSecret;
+        ACCOUNTTTOKEN = accountToken;
     }
 
     /**
      * 获取用户信息接口
-     * @param appUid    第三方应用APP_ID
      * @param code      临时登录凭证
-     * @param appSecret AppSecre
      * @return
      */
-    public  String QueryOpenid(String appUid, String code,String appSecret){
-        Map<String, Object> params = new HashMap<String, Object>();
-        params.put("appUid", appUid);
-        params.put("code", code);
-        params.put("appSecret", appSecret);
-        long timeStamp = System.currentTimeMillis();
-        params.put("timeStamp", timeStamp);
-        String nonceStr = RandomStringUtils.randomAlphanumeric(8);
-        params.put("nonceStr", nonceStr);
-        String param = "app_uid=" + appUid + "&code=" + code + "&time_stamp=" + timeStamp + "&nonce_str=" + nonceStr + "&app_secret=" + appSecret;
-        String computed_sign = HmacSHA256.sign(param, appSecret);
-        params.put("sign", computed_sign);
+    public String queryOpenid(String code){
+        LoggerUtil.info("[queryOpenid] code:{"+code+"},appUid:{"+APPUID+"},accountToken:{"+ACCOUNTTTOKEN+"}");
         String ret = "";
+        Map<String,Object> params = queryParams(code);
         String jsonParam= JSONObject.toJSONString(params);
         try{
             ret = HttpClient.postDataJson(SERVER_ADDRESS+QueryOpenid,jsonParam);
@@ -103,14 +76,31 @@ public class OpenSDk {
             return getMyError("101111", "请求异常");
         }
         return ret;
+    }
 
+    /**
+     * 拼接参数
+     * @param code
+     * @return
+     */
+    public Map<String,Object> queryParams(String code){
+        Map<String, Object> params = new HashMap<String, Object>();
+        String nonceStr = RandomStringUtils.randomAlphanumeric(8);
+        long timeStamp = System.currentTimeMillis();
+        params.put("appUid", APPUID);
+        params.put("code", code);
+        params.put("appSecret", APPSECRET);
+        params.put("timeStamp", timeStamp);
+        params.put("nonceStr", nonceStr);
+        String param = "app_uid=" + APPUID + "&code=" + code + "&time_stamp=" + timeStamp + "&nonce_str=" + nonceStr + "&app_secret=" + APPSECRET;
+        String computed_sign = HmacSHA256.sign(param, APPSECRET);
+        params.put("sign", computed_sign);
+        return params;
     }
 
 
     /**
      * 创建订单
-     * @param appUid     第三方应用APP_ID
-     * @param appSecret  密钥
      * @param openUid    用户OpenId
      * @param orderSn    订单号
      * @param totalFee   价格
@@ -118,12 +108,12 @@ public class OpenSDk {
      * @param notifyUrl  推送回掉地址
      * @return
      */
-    public  String createOrder(String appUid, String appSecret, String openUid,
+    public String createOrder(String openUid,
                               String orderSn, BigDecimal totalFee,String exchange,
                               String body,String notifyUrl,String feeType){
-
+        LoggerUtil.info("[createOrder] openUid:{"+openUid+"},appUid:{"+APPUID+"},accountToken:{"+ACCOUNTTTOKEN+"},orderSn{"+orderSn+"},totalFee:{"+totalFee+"},exchange:{"+exchange+"},body:{"+body+"},notifyUrl:{"+notifyUrl+"}");
         Map<String, Object> params = new HashMap<String, Object>();
-        params.put("appUid", appUid);
+        params.put("appUid", APPUID);
         params.put("openUid", openUid);
         params.put("outTradeNo", orderSn);
         long timeStamp = System.currentTimeMillis();
@@ -135,17 +125,7 @@ public class OpenSDk {
         params.put("nonceStr", nonceStr);
         params.put("body", body);
         params.put("notify_url", notifyUrl);
-
-        String signString = "app_uid=" + appUid +
-                "&open_uid=" + openUid +
-                "&out_trade_no=" + orderSn +
-                "&time_stamp=" + timeStamp +
-                "&total_fee=" + totalFee +
-                "&fee_type=" +feeType+
-                "&exchange=" +exchange+
-                "&nonce_str=" + nonceStr +
-                "&app_secret=" + appSecret;
-        String sign = HmacSHA256.sign(signString, appSecret);
+        String sign = createOrderSign(openUid,orderSn,totalFee,exchange,String.valueOf(timeStamp),nonceStr,feeType);
         params.put("sign", sign);
         String jsonParam=  JSONObject.toJSONString(params);
         String ret = "";
@@ -157,10 +137,93 @@ public class OpenSDk {
             return getMyError("101111", "请求异常");
         }
         return ret;
-
     }
 
+    /**
+     *  创建订单签名
+     * @param openUid    用户OpenId
+     * @param orderSn    订单号
+     * @param totalFee   价格
+     * @param exchange   是否支持币种兑换
+     * @param feeType    币种
+     * @return
+     */
+    public String createOrderSign(String openUid,
+                                  String orderSn, BigDecimal totalFee,String exchange,
+                                  String timeStamp,String nonceStr,String feeType){
+        String signString = "app_uid=" + APPUID +
+                "&open_uid=" + openUid +
+                "&out_trade_no=" + orderSn +
+                "&time_stamp=" + timeStamp +
+                "&total_fee=" + totalFee +
+                "&fee_type=" +feeType+
+                "&exchange=" +exchange+
+                "&nonce_str=" + nonceStr +
+                "&app_secret=" + APPSECRET;
+        String sign = HmacSHA256.sign(signString, APPSECRET);
+        return sign;
+    }
 
+    /**
+     * 商户订单数据校验
+     * @param outTradeNo  订单号
+     * @param totalFee    价格
+     * @param feeType     币种
+     * @param payAt       支付时间
+     * @param result
+     * @param timestamp   时间戳
+     * @param scode
+     * @param sign
+     * @return
+     */
+    public boolean notifyValidate(String outTradeNo,String totalFee, String feeType,String payAt,String result, String timestamp, String scode,String sign){
+        LoggerUtil.info("[notifyValidate] outTradeNo:{"+outTradeNo+"},appUid:{"+APPUID+"},accountToken:{"+ACCOUNTTTOKEN+"},result{"+result+"},totalFee:{"+totalFee+"},payAt:{"+payAt+"},scode:{"+scode+"},sign:{"+sign+"}");
+        boolean flag = true;
+        if(StringUtils.isEmpty(APPUID)) {
+            LoggerUtil.error("必选参数:appUid 为空");
+            throw new IllegalArgumentException("必选参数:appUid 为空");
+        }
+        if(StringUtils.isEmpty(outTradeNo)) {
+            LoggerUtil.error("必选参数:outTradeNo 为空");
+            throw new IllegalArgumentException("必选参数:outTradeNo 为空");
+        }
+        if(StringUtils.isEmpty(totalFee)) {
+            LoggerUtil.error("必选参数:totalFee 为空");
+            throw new IllegalArgumentException("必选参数:totalFee 为空");
+        }
+        if(StringUtils.isEmpty(feeType)) {
+            LoggerUtil.error("必选参数:feeType 为空");
+            throw new IllegalArgumentException("必选参数:feeType 为空");
+        }
+        if(StringUtils.isEmpty(payAt)) {
+            LoggerUtil.error("必选参数:payAt 为空");
+            throw new IllegalArgumentException("必选参数:payAt 为空");
+        }
+        if(StringUtils.isEmpty(result)) {
+            LoggerUtil.error("必选参数:result 为空");
+            throw new IllegalArgumentException("必选参数:result 为空");
+        }
+        if(StringUtils.isEmpty(timestamp)) {
+            LoggerUtil.error("必选参数:timestamp 为空");
+            throw new IllegalArgumentException("必选参数:timestamp 为空");
+        }
+        if(StringUtils.isEmpty(scode)) {
+            LoggerUtil.error("必选参数:scode 为空");
+            throw new IllegalArgumentException("必选参数:scode 为空");
+        }
+        if(StringUtils.isEmpty(sign)) {
+            LoggerUtil.error("必选参数:sign 为空");
+            throw new IllegalArgumentException("必选参数:sign 为空");
+        }
+        if (!verifyNotify(APPUID, outTradeNo,
+                totalFee, feeType, payAt,
+                timestamp, scode,
+                sign, APPSECRET)) {
+            LoggerUtil.error("签名验证错误 ");
+            flag = false;
+        }
+        return  flag;
+    }
 
 
     private static String getMyError(String code, String msg) {
@@ -184,7 +247,35 @@ public class OpenSDk {
         return mHttpRequestBase;
     }
 
+    /**
+     * 订单签名
+     * @param appUid
+     * @param outTradeNo
+     * @param totalFee
+     * @param feeType
+     * @param payAt
+     * @param timestamp
+     * @param scode
+     * @param sign
+     * @param appSecret
+     * @return
+     */
+    public Boolean verifyNotify(String appUid, String outTradeNo,
+                                String totalFee, String feeType, String payAt,
+                                String timestamp, String scode,
+                                String sign, String appSecret) {
+        String params = "app_uid=" + appUid +
+                "&out_trade_no=" + outTradeNo +
+                "&total_fee=" + totalFee +
+                "&fee_type=" + feeType +
+                "&pay_at=" + payAt +
+                "&timestamp=" + timestamp +
+                "&scode=" + scode +
+                "&app_secret=" + appSecret;
+        String computedSign = HmacSHA256.sign(params, appSecret);
 
+        return sign.equals(computedSign);
+    }
 
     private void setHttpHeader(AbstractHttpMessage httpMessage) {
         httpMessage.setHeader("Accept", "application/json");
--- src/main/java/com/sdk/demo/OpenSdkDemo.java
+++ src/main/java/com/sdk/demo/OpenSdkDemo.java
@@ -16,10 +16,7 @@ public class OpenSdkDemo {
 
     public static void main(String[] args) {
         String result ="";
-        String appUid ="34342fsdfsdf";
         String accountUid = "171";
-        String accountToken= "eyJhbGciOiJIUzI1NiJ9.eyJ0ZWxlZ3JhbUlkIjoiMzM0ODAyMDk2In0.aKDrqbmGmKVaADCC08M1NjiIw9lcSwPK40PBaEpKc-Q";
-        String appSecret = "6aae383b54f5d3503097991a60575bd7";
         String openUid="7e92616c0d0d4c1987e38bb80a6258c0";
         String orderSn="HVcS3x6xB1qf0vcOxx";
         BigDecimal totalFee=new BigDecimal(0);
@@ -27,53 +24,54 @@ public class OpenSdkDemo {
         String notifyUrl="www.jd.com";
         String exchange = "true";
         String feeType = "RMB";
-
-
+        String outTradeNo = "";
+        String payAt = "";
+        String timestamp="";
+        String scode ="";
+        String code ="";
+        String sign="";
         //初始化SDK
         OpenSDk openSDk = new OpenSDk();
 
         //******************************注释*********************************************
-        //*初始化服务器地址                                                              *
-        //*生产环境（用户应用上线使用）：openSDk.init("http://127.0.0.1:8086");           *
+        //*serveraddress 初始化服务器地址                                                 *
+        //*openUid    用户OpenId                                                         *
+        //*appUid    第三方应用APP_ID                                                    *
+        //*appSecret  密钥                                                               *
         //*******************************************************************************
-        openSDk.init("http://127.0.0.1:8086");
-        //******************************注释*********************************************
-        //*获取用户登陆凭证                                                              *
-        //*appUid       第三方应用APP_ID                                                 *
-        //*accountUid   App账号ID                                                        *
-        //*accountToken App账号当前Token                                                 *
-        //*******************************************************************************
-        result = openSDk.queryCode(appUid,accountUid,accountToken);
-        System.out.println("result:"+result);
-        JSONObject jsonObject=JSONObject.parseObject(result);
-        if("0".equals(jsonObject.get("statusCode"))){
-            String code = jsonObject.getJSONObject("data").getString("code");
-            System.out.println("statusCode=" + jsonObject.get("statusCode") +" statusMsg= "+jsonObject.get("statusMsg") +"code:"+code);
-            //正常返回
-        }else{
-            //异常返回输出错误码和错误信息
-            System.out.println("错误码=" + jsonObject.get("statusCode") +" 错误信息= "+jsonObject.get("statusMsg"));
-        }
+        openSDk.init("http://127.0.0.1:8086","34342fsdfsdf","eyJhbGciOiJIUzI1NiJ9.eyJ0ZWxlZ3JhbUlkIjoiMzM0ODAyMDk2In0.aKDrqbmGmKVaADCC08M1NjiIw9lcSwPK40PBaEpKc-Q","6aae383b54f5d3503097991a60575bd7");
+
         //******************************注释*********************************************
         //*获取用户信息接口                                                              *
         //*appUid    第三方应用APP_ID                                                    *
         //*code      临时登录凭证                                                        *
         //*appSecret 密钥                                                                *
         //*******************************************************************************
-        result = openSDk.QueryOpenid(appUid,jsonObject.getJSONObject("data").getString("code"),appSecret);
+        result = openSDk.queryOpenid(code);
         System.out.println("result:"+result);
 
         //******************************注释*********************************************
         //*创建订单                                                                      *
-        //*appUid    第三方应用APP_ID                                                    *
-        //*openUid    用户OpenId                                                         *
         //*orderSn    订单号                                                             *
         //*totalFee   价格                                                               *
         //*body       描述                                                               *
         //*notifyUrl  推送回掉地址                                                        *
         //*appSecret  密钥                                                                *
         //*******************************************************************************
-        result = openSDk.createOrder(appUid,appSecret,openUid,orderSn,totalFee,exchange,body,notifyUrl,feeType);
+        result = openSDk.createOrder(openUid,orderSn,totalFee,exchange,body,notifyUrl,feeType);
         System.out.println("result:"+result);
+
+        //******************************注释*********************************************
+        //*商户订单数据校验                                                               *
+        //*outTradeNo    订单号                                                          *
+        //*totalFee      价格                                                            *
+        //*feeType       币种                                                            *
+        //*payAt         支付时间                                                        *
+        //*result                                                                        *
+        //*timestamp     时间戳                                                          *
+        //*scode                                                                         *
+        //*sign          签名                                                            *
+        //*******************************************************************************
+        boolean flag = openSDk.notifyValidate(outTradeNo,totalFee.toString(),feeType,payAt,result,timestamp, scode, sign);
     }
 }
